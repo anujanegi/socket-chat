f25e1ce02fde1f88b921ae1c8c3ba6d442d146eb
anujanegi <anujanegi1@gmail.com>
2018-01-05 12:29:45 +0530

    Emitting events added

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log("User connected.");
+io.on('connection', function (socket) {
+  console.log("-------------------------A user connected-------------------------");
+  socket.on('disconnect', function () {
+    console.log('-------------------------A user disconnected-------------------------');
+  });
+  socket.on('chat message', function (msg) {
+    console.log('Message:' + msg);
+  });
 });
 
 http.listen(8080, function(){
